2468cf408380914d7c8e71fb741757ae14dca603
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-10-05 13:22:53 -0500

    Create AppData.java

---
+++ AppData.java
@@ -0,0 +1,12 @@
+package com.example.ucm;
+
+public class AppData {
+    public static String[] nameArray = {"YouTube", "Google", "Post Coffee Co", "AccuWeather",
+            "RideKC", "Seattle", "Lee's Summit", "Kansas City"};
+
+    public static Integer[] drawable = {R.drawable.youtube, R.drawable.googleicon, R.drawable.postcoffeeicon,
+        R.drawable.accuweathericon, R.drawable.ridekclogo, R.drawable.seattlelogo,
+        R.drawable.lslogo, R.drawable.kansascitylogo};
+
+    public static Integer[] id = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
