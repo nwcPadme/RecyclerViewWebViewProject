a4500d19a6ae4fe5d88c88184faf95f17e587cdd
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-10-05 17:23:23 -0500

    Create WebViewFragment.java

---
+++ WebViewFragment.java
@@ -0,0 +1,59 @@
+package com.example.ucm;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewFragment extends Fragment {
+
+    public static String mURL = "https://www.youtube.com/";
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.web_view, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if(savedInstanceState != null) {
+            mURL = savedInstanceState.getString("currentURL", "");
+
+            if(!mURL.equals("")) {
+                WebView webView = getView().findViewById(R.id.pageInfo);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl(mURL);
+            }
+        }
+    }
+
+    public void savedInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("currentURL", mURL);
+    }
+
+    public void setURLContent(String URL) {
+        mURL = URL;
+    }
+
+    public void updateURLContent(String URL) {
+        mURL = URL;
+        WebView webView = getView().findViewById(R.id.pageInfo);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl(mURL.trim());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return false; // Let our WebView to load the page.
+        }
+    }
+}
