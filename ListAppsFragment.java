bcd10847a4f40d389c34e758ef30c025b45e84ee
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-10-06 12:03:49 -0500

    Update ListAppsFragment.java

--- ListAppsFragment.java
+++ ListAppsFragment.java
@@ -1,5 +1,7 @@
 package com.example.ucm;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,44 +13,120 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ListAppsFragment extends RecyclerView.Adapter<ListAppsFragment.ViewHolder> {
+public class ListAppsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private ListAppsAdapter mAdapter;
+    OnURLSelectedListener mListener;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textViewName;
-        ImageView imageView;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            textViewName = itemView.findViewById(R.id.text_view);
-            imageView    = itemView.findViewById(R.id.image_view);
-        }
-    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_apps_list, container, false);
 
-    private ArrayList<AppModel> dataset;
+        mRecyclerView = view.findViewById(R.id.app_list_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-    public ListAppsFragment(ArrayList<AppModel> data) {
-        dataset = data;
+        updateUI();
+
+        return view;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.apps_layout, parent, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-        //view.setOnClickListener(ListAppsActivity.myOnClickListener);
-
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        //displayRecyclerView();
     }
 
-    public void onBindViewHolder(final ViewHolder holder, final int listPosition) {
-        TextView textViewName = holder.textViewName;
-        ImageView imageView   = holder.imageView;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
 
-        textViewName.setText(dataset.get(listPosition).getName());
-        imageView.setImageResource(dataset.get(listPosition).getImage());
+        if(context instanceof Activity) {
+            /*
+            try {
+                mListener = (OnURLSelectedListener) context;
+            } catch (ClassCastException e) {
+                throw new ClassCastException(context.toString() + " must implement OnURLSelectedListener");
+            }
+            */
+        }
     }
 
-    public int getItemCount() {
-        return dataset.size();
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    private void updateUI() {
+        AppList appList = AppList.get(getActivity());
+        List<AppModel> appModel = appList.getApps();
+
+        if(mAdapter == null) {
+            mAdapter = new ListAppsAdapter(appModel);
+            mRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private class AppListHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private AppModel mApp;
+        private TextView mTextView;
+        private ImageView mImageView;
+
+        public void bind(AppModel app) {
+            mApp = app;
+            mTextView.setText(mApp.getName());
+            mImageView.setImageResource(mApp.getImage());
+        }
+
+        public AppListHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.apps_layout, parent, false));
+            itemView.setOnClickListener(this);
+
+            mTextView  = itemView.findViewById(R.id.text_view);
+            mImageView = itemView.findViewById(R.id.image_view);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = WebViewActivity.newIntent(getActivity(), mApp.getID());
+            startActivity(intent);
+        }
+    }
+
+    private class ListAppsAdapter extends RecyclerView.Adapter<AppListHolder> {
+
+        public List<AppModel> data;
+
+        public ListAppsAdapter(List<AppModel> apps) {
+            data = apps;
+        }
+
+        @Override
+        public AppListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+            return new AppListHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(AppListHolder holder, int position) {
+            AppModel app = data.get(position);
+            holder.bind(app);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+    }
+
+    public interface OnURLSelectedListener {
+        public void onURLSelected(String URL);
     }
 }
