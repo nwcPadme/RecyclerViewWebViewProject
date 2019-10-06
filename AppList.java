7197f9ed4898402db9642c5eeb5e84572a688bc2
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-10-06 12:00:05 -0500

    Create AppList.java

---
+++ AppList.java
@@ -0,0 +1,37 @@
+package com.example.ucm;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppList {
+    private static AppList sAppList;
+    private Context mContext;
+    private List<AppModel> mAppLists;
+
+    public static AppList get(Context context) {
+        if(sAppList == null) {
+            sAppList = new AppList(context);
+        }
+
+        return sAppList;
+    }
+
+    private AppList(Context context) {
+        mContext = context.getApplicationContext();
+        mAppLists = new ArrayList<>();
+        for(int i = 0; i < AppData.nameArray.length; i++) {
+            mAppLists.add(new AppModel(
+                    AppData.nameArray[i],
+                    AppData.URLS[i],
+                    AppData.id[i],
+                    AppData.drawable[i]
+            ));
+        }
+    }
+
+    public List<AppModel> getApps() {
+        return new ArrayList<>(mAppLists);
+    }
+}
