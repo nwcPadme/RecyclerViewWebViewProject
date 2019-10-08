b434c35ebf1c51065082c207ae19645c1cbf8c99
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-10-08 14:10:33 -0500

    Update WebViewActivity.java

--- WebViewActivity.java
+++ WebViewActivity.java
@@ -6,16 +6,18 @@ import android.support.v4.app.Fragment;
 
 public class WebViewActivity extends SingleFragmentActivity {
 
-    private static final String EXTRA_APP_ID = "com.example.ucm.app_id";
+    private static final String EXTRA_APP_URL = "com.example.ucm.app_url";
 
     @Override
     protected Fragment createFragment() {
+        //String URL = getIntent().getStringExtra(EXTRA_APP_URL);
+        //return new WebViewFragment().newInstance(URL);
         return new WebViewFragment();
     }
 
-    public static Intent newIntent(Context packageContext, int appID) {
+    public static Intent newIntent(Context packageContext, String appURL) {
         Intent intent = new Intent(packageContext, WebViewActivity.class);
-        intent.putExtra(EXTRA_APP_ID, appID);
+        intent.putExtra(EXTRA_APP_URL, appURL);
         return intent;
     }
 }
